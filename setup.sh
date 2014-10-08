f6d4b75fa6693087cbb52d8a253b32482252233a
spuduk <mail@stewartspurrier.co.uk>
2014-10-08 18:40:40 +0100

    modified:   setup.sh 	renamed:    Home.conf -> setup/Home.conf 	renamed:    tmp/HomeAutomation.php -> setup/HomeAutomation.php 	renamed:    tmp/config.json -> setup/config.json 	new file:   setup/init 	new file:   setup/settings.json

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-/bin/bash
+#!/bin/bash
 
-sudo echo "deb mirror://apt.pilight.org/mirror.txt stable main" >> /etc/apt/sources.list
-sudo wget -O - http://apt.pilight.org/pilight.key | sudo apt-key add -
-sudo apt-get -y update
-sudo apt-get -y install pilight
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+
+#sudo echo "deb mirror://apt.pilight.org/mirror.txt stable main" >> /etc/apt/sources.list
+#sudo wget -O - http://apt.pilight.org/pilight.key | sudo apt-key add -
+#sudo apt-get -y update
+#sudo apt-get -y install pilight apache2
+
+sudo cp setup/config.json /etc/pilight/
+sudo cp setup/settings.json /etc/pilight/
+sudo cp setup/HomeAutomation.php /home/pi/
+sudo cp setup/init /etc/init/Home.conf
