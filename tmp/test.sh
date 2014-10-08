e871ce97cb5146d18c75d71a9a9f4121e34a7a7d
Spud <mail@stewartspurrier.co.uk>
2014-10-08 18:43:48 +0100

    deleted:    tmp/Automation.log 	deleted:    tmp/HA.sh 	deleted:    tmp/HA/Header.sh 	deleted:    tmp/HA/ha.sh 	deleted:    tmp/HA/light.sh 	deleted:    tmp/HomeAutomation_bak.php 	deleted:    tmp/Light.sh 	deleted:    tmp/Test.log 	deleted:    tmp/cron.json 	deleted:    tmp/homeautomation.conf 	deleted:    tmp/monitor.php 	deleted:    tmp/test.sh 	deleted:    tmp/wake.sh 	deleted:    tmp/www/config.php 	deleted:    tmp/www/error.php 	deleted:    tmp/www/ha.php 	deleted:    tmp/www/ha.php.save 	deleted:    tmp/www/index.html 	deleted:    tmp/www/index.php 	deleted:    tmp/www/lights.php 	deleted:    tmp/www/mediacentre.php 	deleted:    tmp/www/tesh.html 	deleted:    tmp/www/test.php

--- tmp/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lightstatus=$(jq -r .living.lamp.state /etc/pilight/config.json)
-echo $lightstatus
--- tmp/www/error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function Error($Err){
-        echo "function Error($Err)<br>";
-	echo "Error incountered with ".$Err." option";
-	break;
-}
-
-?>
