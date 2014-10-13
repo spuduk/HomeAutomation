b4f5edaf6fbc2aab64ccaae7808df8ef2433084e
Spud <mail@stewartspurrier.co.uk>
2014-10-13 22:41:42 +0100

    new file:   reset.sh 	modified:   setup.sh 	deleted:    setup/init

---
+++ reset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+User="pi"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+USER_HOME=$(eval echo ~${SUDO_USER})
+InsDir=$USER_HOME"/HomeAutomation"
+rm $InsDir"/HomeAutomation.php"
+rm $InsDir"/lights.php"
+rm /etc/pilight/config.json
+rm /etc/pilight/settings.json
+rm /etc/init/Home.conf
+rmdir $InsDir
+
--- setup.sh
+++ setup.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
+User="pi"
 
-#sudo apt-get -y update
-#sudo apt-get -y upgrade
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-#sudo echo "deb mirror://apt.pilight.org/mirror.txt stable main" >> /etc/apt/sources.list
-#sudo wget -O - http://apt.pilight.org/pilight.key | sudo apt-key add -
-#sudo apt-get -y update
-#sudo apt-get -y install pilight apache2
+echo "Preforming Apt update & upgrade"
+#apt-get -y update > /dev/null
+#apt-get -y upgrade > /dev/null
 
-sudo cp setup/config.json /etc/pilight/
-sudo cp setup/settings.json /etc/pilight/
-sudo cp setup/HomeAutomation.php /home/pi/
-sudo cp setup/lights.php /home/pi/HomeAutomation/
-sudo cp setup/init /etc/init/Home.conf
-sudo chmod +x /etc/init/Home.conf
+apt="$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep pilight |wc -l)"
+if [ $apt -ge 1 ]; then
+	echo "Respository already installed"
+else
+	echo "Adding Repository"
+#	echo "deb mirror://apt.pilight.org/mirror.txt stable main" >> /etc/apt/sources.list
+#	wget -O - http://apt.pilight.org/pilight.key | sudo apt-key add -
+#	apt-get -y update
+fi
 
+echo "Install PiLight and Apache2"
+apt-get -y --force-yes install pilight apache2 upstart
+
+
+echo "Create Install Directory"
+
+USER_HOME=$(eval echo ~${SUDO_USER})
+InsDir=$USER_HOME"/HomeAutomation"
+
+#if [ ! -e $InsDir ]; then
+	sudo -u pi mkdir $InsDir
+	echo "Create DIR"
+#else
+#	echo "DIR Exists"
+#fi
+
+
+echo "Copying HomeAutomation files"
+InsFile=$InsDir"/HomeAutomation.php"
+
+#if [ -e $InsFile ]; then
+#	echo "Already installed"
+#else
+	echo "Copying"
+	cp setup/config.json /etc/pilight/
+	cp setup/settings.json /etc/pilight/
+	sudo -u pi cp setup/HomeAutomation.php $InsDir
+	sudo -u pi cp setup/lights.php $InsDir
+fi
+
+echo "Create init script"
+if [ -e "/etc/init/Home.conf" ]; then
+	echo "init script exists"
+else
+	echo "Creating init script"
+	echo "description 'A script controlled by upstart'" > /etc/init/Home.conf
+	echo "author 'Spud'" >> /etc/init/Home.conf
+	echo "" >> /etc/init/Home.conf
+	echo "start on started mountall" >> /etc/init/Home.conf
+	echo "stop on shutdown" >> /etc/init/Home.conf
+	echo "" >> /etc/init/Home.conf
+	echo "script" >> /etc/init/Home.conf
+	echo "  export HOME='/home/pi'">> /etc/init/Home.conf
+	echo "  exec /usr/bin/php "$InsFile" >> /var/log/HomeAutomation.log 2>&1" >> /etc/init/Home.conf
+	echo "end script" >> /etc/init/Home.conf
+
+fi
+
+echo "Applying executable tags"
+chmod +x /etc/init/Home.conf
